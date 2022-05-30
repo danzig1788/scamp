c6f957050e2511911edf7233acc0851cb2ec1ae1
Kyle Mcloughlin <Kyle1860@gmail.com>
2022-05-30 14:34:09 +0200

    Added simpsons tune

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
+# GitHub token_apollo: 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
+
 from scamp import *
 
 s = Session()
+s.tempo = 120
 
 #s.print_default_soundfont_presets()
 
 clarinet = s.new_part('clarinet')
 oboe = s.new_part('oboe')
 
+# Set up a series of pitches and corresponding durations
+pitch_list = [60,64,66,69,67,64,60,57,54,54,54,55]
+durs_list = [1.5,1.0,1.0,0.5,1.5,1.0,1.0,0.5,0.5,0.5,0.5,0.5]
+
+
 # Play note takes 3 arguments: 1=midi pitch; 2=vol (range is 0 to 1): 3=duration (in seconds by default)
-for pitch in range(65,77):
-    clarinet.play_note(pitch,.8,.25)
-    oboe.play_note(pitch,.8,.25)
+for pitch, duration in zip(pitch_list,durs_list):
+    clarinet.play_note(pitch,.8,duration)
+    #oboe.play_note(pitch,.8,duration)
