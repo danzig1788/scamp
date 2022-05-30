c9b0958064a66d6fbd8477e3a413de3e00f3b753
Kyle Mcloughlin <Kyle1860@gmail.com>
2022-05-30 14:43:29 +0200

    Removed github details

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# GitHub token_apollo: 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
+
 
 from scamp import *
 
@@ -15,7 +15,8 @@ pitch_list = [60,64,66,69,67,64,60,57,54,54,54,55]
 durs_list = [1.5,1.0,1.0,0.5,1.5,1.0,1.0,0.5,0.5,0.5,0.5,0.5]
 
 
-# Play note takes 3 arguments: 1=midi pitch; 2=vol (range is 0 to 1): 3=duration (in seconds by default)
+# Play note takes 3 required arguments: 1=midi pitch; 2=vol (range is 0 to 1): 3=duration (in seconds by default)
+# and an optional, all-purpose, 4th argument, "properties"
 for pitch, duration in zip(pitch_list,durs_list):
-    clarinet.play_note(pitch,.8,duration)
+    clarinet.play_note(pitch,.8,duration, 'staccato')
     #oboe.play_note(pitch,.8,duration)
